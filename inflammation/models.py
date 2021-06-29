b4eaf69a2310dfa6b942c5a412a41a4ce44173a0
aleighbrown <brown.annaleigh@gmail.com>
2021-06-29 14:41:45 +0100

    making her prettier

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,17 +19,26 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2d inflammation data array.
+    :param data: numpy array where rows are patients and columns are days
+    :returns: mean value across patients measurement per day
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2d inflammation data array.
+    :param data: numpy array where rows are patients and columns are days
+    :returns: an array of max value across patients measurement per day
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2d inflammation data array."""
+    """Calculate the daily min of a 2d inflammation data array.
+    :param data: numpy array where rows are patients and columns are days
+    :returns: an array of min value across patients measurement per day
+    """
     return np.min(data, axis=0)
 
 
